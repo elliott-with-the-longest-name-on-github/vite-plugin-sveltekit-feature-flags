59c1bce09bd09af4db363755a574dfda22a3560a
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-08-23 16:33:32 -0600

    Initial commit

---
+++ packages/vite-plugin-template/tsup.config.js
@@ -0,0 +1,9 @@
+/** @type {import("tsup").Options} */
+export const tsup = {
+	entryPoints: ['src/index.ts'],
+	format: ['esm'],
+	sourcemap: true,
+	splitting: false,
+	clean: true,
+	target: 'node14.18.0'
+};
